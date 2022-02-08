32c5487bd8160f4e0e2b470f551e3f6f2279e945
Michael Mumenthaler <michi.mumi@gmail.com>
2022-02-08 22:01:16 +0100

    create notification card, implement notification cards in notification page
    Signed-off-by: Michael Mumenthaler <michi.mumi@gmail.com>

--- src/Components/Notifications.tsx
+++ src/Components/Notifications.tsx
@@ -1,8 +1,9 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { useState, useContext, useEffect } from "react";
 import { AuthenticationContext } from "../App";
 import FloatingTenantChange from "./Items/FloatingTenantChange";
 import Navbar from "./Navbar";
+import MessageCard from "./Items/CardComponents/Notifications/MessageCard";
 
 export default function Notifications() {
   const [notifications, setNotifications] = useState([]);
@@ -17,7 +18,7 @@ export default function Notifications() {
     }).then((res) => {
       if (res.status === 200) {
         res.json().then((jsonObj) => {
-          console.log(jsonObj);
+          //console.log(jsonObj);
           if (jsonObj) {
             setNotifications(jsonObj);
           } else {
@@ -36,12 +37,25 @@ export default function Notifications() {
       <Typography variant="h2" component="div" gutterBottom>
         Benachrichtigungen
       </Typography>
-      <div>
-        {notifications.map((notification, index) => (
-          <p key={index}>{notification}</p>
-        ))}
-      </div>
-      <FloatingTenantChange />
+      <Stack
+        direction="column"
+        spacing={5}
+        justifyContent="center"
+        alignItems="center"
+      >
+        {notifications
+          ? notifications.map((message: any, index) => (
+              <MessageCard
+                key={message.client_msg_id}
+                message={message.message}
+                messageLink={message.link_to_message}
+                timestamp={message.unix_timestamp}
+                userAvatarUrl={message.user_avatar_url}
+                userRealName={message.user_real_name}
+              />
+            ))
+          : "NO MESSAGES"}
+      </Stack>
     </>
   );
 }
